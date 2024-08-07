4c8172c62602a46299806a853d0287218e556f1d
Nerk0s <nrek0siii@gmail.com>
2024-08-07 12:46:42 +0200

    development start

---
+++ pdf_config_creator.py
@@ -0,0 +1,60 @@
+# Creates a configuration file which contains the coordinates of the text fields to extract from pdf files
+# The configuration file is a json file which contains the following fields:
+# - character name
+# - class and Level
+# - player name
+# - race
+# - background
+# - experience points
+# - abilities
+#   - strength
+#   - dexterity
+#   - constitution
+#   - intelligence
+#   - wisdom
+#   - charisma
+# - saving throws
+#   - strength
+#   - dexterity
+#   - constitution
+#   - intelligence
+#   - wisdom
+#   - charisma
+# - skills
+#   - acrobatics
+#   - animal handling
+#   - arcana
+#   - athletics
+#   - deception
+#   - history
+#   - insight
+#   - intimidation
+#   - investigation
+#   - medicine
+#   - nature
+#   - perception
+#   - performance
+#   - persuasion
+#   - religion
+#   - sleight of hand
+#   - stealth
+#   - survival
+#   - and any other skill
+# - senses
+#   - passive perception
+#   - passive investigation
+#   - passive insight
+# - armor class
+# - initiative
+# - inspiration
+# - proficiency bonus
+# - ability save dc
+# - speed
+# - Defenses
+#   - hit points
+#   - hit dice
+#   - death saves
+#   - temporary hit points
+# - actions will be set manually
+# - weapion attacks and cantrips
+# - spells
